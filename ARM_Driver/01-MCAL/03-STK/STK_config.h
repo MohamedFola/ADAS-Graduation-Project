0c1823c56d5f9ee5e29a80c86bc1bfdd0816e431
MohabAhmed1 <93516494+MohabAhmed1@users.noreply.github.com>
2023-02-01 18:18:33 +0200

    Systick v1.0 by Mohab and Bassiony

---
+++ ARM_Driver/01-MCAL/03-STK/STK_config.h
@@ -0,0 +1,16 @@
+/*
+ * STK_config.h
+ *
+ *  Created on: Feb 1, 2023
+ *      Author: speedTECH
+ */
+
+#ifndef STK_CONFIG_H_
+#define STK_CONFIG_H_
+/*
+ * Choose:
+ * 1.AHB
+ * 2.AHB_DIVIDE_BY_8*/
+#define STK_CLK_SRC AHB_DIVIDE_BY_8
+
+#endif /* STK_CONFIG_H_ */
