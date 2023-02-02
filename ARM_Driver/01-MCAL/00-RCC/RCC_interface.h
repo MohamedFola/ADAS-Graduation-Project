ce5e44b49d806beeaabf04ba96f08f731e4d09f9
Mohamed Magdy <86077031+MohamedMagdyJarrah@users.noreply.github.com>
2023-02-02 13:08:09 +0200

    Adding Peripherals IDs

--- ARM_Driver/01-MCAL/00-RCC/RCC_interface.h
+++ ARM_Driver/01-MCAL/00-RCC/RCC_interface.h
@@ -32,6 +32,40 @@
 #define RCC_TIM10	20
 #define RCC_TIM11	21
 
+//Peripherals you choose when you enable it for APB1 bus
+#define RCC_TIM2    0
+#define RCC_TIM3    1
+#define RCC_TIM4    2
+#define RCC_TIM5    3
+#define RCC_TIM6    4
+#define RCC_TIM7   	5
+#define RCC_TIM12	6
+#define RCC_TIM13   7
+#define RCC_TIM14   8
+#define RCC_WWDG   	11
+#define RCC_SPI2    14
+#define RCC_SPI3    15
+#define RCC_USART2  17
+#define RCC_USART3  18
+#define RCC_USART4  19
+#define RCC_USART5  20
+#define RCC_I2C1    21
+#define RCC_I2C2    22
+#define RCC_USB     23
+#define RCC_CAN     25
+#define RCC_BKP     27
+#define RCC_PWR     28
+#define RCC_DAC     29
+
+//Peripherals you choose when you enable it for AHB bus
+#define RCC_DMA1	0
+#define RCC_DMA2	1
+#define RCC_SRAM	2
+#define RCC_FLITF	4
+#define RCC_CRCE	6
+#define RCC_FSMC	8
+#define RCC_SDIO	10
+
 
 void RCC_voidInitSysClock(void);
 
