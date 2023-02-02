aa841dd6671b866d73435a8b2eb3053baaf96712
Mohamed Magdy <86077031+MohamedMagdyJarrah@users.noreply.github.com>
2023-02-02 12:29:15 +0200

    Adding Peripherals IDs

--- ARM_Driver/01-MCAL/00-RCC/RCC_interface.h
+++ ARM_Driver/01-MCAL/00-RCC/RCC_interface.h
@@ -17,7 +17,20 @@
 #define RCC_GPIOA 	2
 #define RCC_GPIOB 	3
 #define RCC_GPIOC 	4
-
+#define RCC_GPIOD	5
+#define RCC_GPIOE	6
+#define RCC_GPIOF	7
+#define RCC_GPIOG	8
+#define RCC_ADC1	9
+#define RCC_ADC2	10
+#define RCC_TIM1	11
+#define RCC_SPI1	12
+#define RCC_TIM8	13
+#define RCC_USART1	14
+#define RCC_ADC3	15
+#define RCC_TIM9	19
+#define RCC_TIM10	20
+#define RCC_TIM11	21
 
 
 void RCC_voidInitSysClock(void);
