af63cbd740ec315005428f64b46f98d167bf0c6d
Mohamed Magdy <mohamedmagdygarrah@gmail.com>
2023-02-11 22:16:24 +0200

    Changing interface file

--- ARM_Driver/01-MCAL/04-NVIC/NVIC_interface.h
+++ ARM_Driver/01-MCAL/04-NVIC/NVIC_interface.h
@@ -2,37 +2,37 @@
 #define NVIC_INTERFACE_H
 
 /*Interrupt IDs to pass in interrupt number*/
-#define WATCH_DOG				0
-#define PVD						1
-#define TAMPER					2
-#define RTC						3
-#define FLASH					4
-#define RCC						5
-#define EXTI0					6
-#define EXTI1					7
-#define EXTI2					8
-#define EXTI3					9
-#define EXTI4					10
-#define ADC						18
-#define CANRX1					21
-#define CANSCE					22
-#define EXTI9_5					23
-#define TIM1_BRK				24
-#define TIM1_UP					25
-#define TIM1_TRG_COM			26
-#define TIM1_CC					27
-#define TIM2					28
-#define TIM3					29
-#define TIM4					30
-#define I2C1_EV					31
-#define I2C1_ER					32
-#define I2C2_EV					33
-#define I2C2_ER					34
-#define SPI1					35
-#define SPI2					36
-#define USART1					37
-#define USART2					38
-#define USART3					39
+#define NVIC_WATCH_DOG				0
+#define NVIC_PVD					1
+#define NVIC_TAMPER					2
+#define NVIC_RTC					3
+#define NVIC_FLASH					4
+#define NVIC_RCC					5
+#define NVIC_EXTI0					6
+#define NVIC_EXTI1					7
+#define NVIC_EXTI2					8
+#define NVIC_EXTI3					9
+#define NVIC_EXTI4					10
+#define NVIC_ADC					18
+#define NVIC_CANRX1					21
+#define NVIC_CANSCE					22
+#define NVIC_EXTI9_5				23
+#define NVIC_TIM1_BRK				24
+#define NVIC_TIM1_UP				25
+#define NVIC_TIM1_TRG_COM			26
+#define NVIC_TIM1_CC				27
+#define NVIC_TIM2					28
+#define NVIC_TIM3					29
+#define NVIC_TIM4					30
+#define NVIC_I2C1_EV				31
+#define NVIC_I2C1_ER				32
+#define NVIC_I2C2_EV				33
+#define NVIC_I2C2_ER				34
+#define NVIC_SPI1					35
+#define NVIC_SPI2					36
+#define NVIC_USART1					37
+#define NVIC_USART2					38
+#define NVIC_USART3					39
 
 
 /*Functions to Enable or Disable NVIC Interrupt*/
